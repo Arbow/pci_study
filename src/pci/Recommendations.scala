c86fe96dd08828c0c250ebce73bf485c61808357
arbow <avindev+github@gmail.com>
2010-08-20 00:28:17 +0800

    update chapter 1

--- src/pci/Recommendations.scala
+++ src/pci/Recommendations.scala
@@ -2,10 +2,11 @@ package pci
 
 object Algorithms {
   import scala.math._
+  import scala.collection.Map
   import scala.collection.mutable.HashMap
 
   def simDistance(prefs:Map[String, Map[String,Double]], person1:String, person2:String) = {
-    val shareItems = new HashMap[String,Int]()
+    val shareItems = HashMap[String,Int]()
     for {item <- prefs(person1)
          if prefs(person2).contains(item._1)} shareItems(item._1) = 1
     if (shareItems.size == 0) 0
@@ -19,7 +20,7 @@ object Algorithms {
   }
 
   def simPearson(prefs:Map[String, Map[String, Double]], person1:String, person2:String): Double = {
-    val shareItems = new HashMap[String,Int]()
+    val shareItems = HashMap[String,Int]()
     for {item <- prefs(person1)
          if prefs(person2).contains(item._1)} shareItems(item._1) = 1
 
@@ -37,6 +38,36 @@ object Algorithms {
     val num = pSum - (sum1 * sum2 / len)
     val den = sqrt((sum1Sq-pow(sum1,2.0)/len) * (sum2Sq-pow(sum2,2.0)/len))
     if (den == 0) 0 else num/den
+  }
+
+  def topMatcher(prefs:Map[String, Map[String, Double]], person:String, n:Int=5,
+                 similarity:(Map[String, Map[String,Double]],String, String)=>Double = simPearson) = {
+    val scores:Iterable[(Double,String)] = for {other <- prefs.keySet
+                      if other != person} yield (similarity(prefs, person, other), other)
+    scores.toList.sortWith(_._1 > _._1).take(n)
+  }
+
+  def getRecommendations(prefs:Map[String, Map[String, Double]], person:String,
+                         similarity:(Map[String, Map[String,Double]],String, String)=>Double = simPearson) = {
+    val totals = HashMap[String, Double]()
+    val simSums = HashMap[String, Double]()
+
+    for {other <- prefs.keySet
+         if other != person
+         val sim = similarity(prefs, person, other)
+         if sim > 0
+         item <- prefs(other)
+         if !prefs(person).contains(item._1) || prefs(person)(item._1) == 0
+    } {
+      if (!totals.contains(item._1)) totals(item._1) = 0
+      totals(item._1) += prefs(other)(item._1) * sim
+
+      if (!simSums.contains(item._1)) simSums(item._1) = 0
+      simSums(item._1) += sim
+    }
+    
+    val ranking:Iterable[(Double,String)] = (for {(item,total) <- totals} yield (total/simSums(item), item))
+    ranking.toList.sortWith(_._1 > _._1)
   }
 }
 
@@ -57,23 +88,27 @@ object Recommendations {
     "Michael Phillips" -> Map("Lady in the Water" -> 2.5, "Snakes on a Plane" -> 3.0, "Superman Returns" -> 3.5,
                               "The Night Listener" -> 4.0))
 
-  import Algorithms._
-  type Similarity = {def calc(prefs:Map[String, Map[String,Double]], person1:String, person2:String):Double}
-  def topMatcher(prefs:Map[String, Map[String, Double]], person:String, n:Int=5, 
-                  similarity:(Map[String, Map[String,Double]],String, String)=>Double = simPearson) = {
-    val scores:Iterable[(Double,String)] = for {other <- prefs
-                      if other._1 != person} yield (similarity(prefs, person, other._1), other._1)
-//    scores.toList.sortWith({(t1,t2)=>t1._1 > t2._1}).take(n)
-    scores.toList.sortWith(_._1 > _._1).take(n)
+  def transformPrefs(prefs:Map[String, Map[String, Double]]) = {
+    import scala.collection.mutable.HashMap
+    val result = HashMap[String, HashMap[String,Double]]()
+    for {person <- prefs.keySet; item <- prefs(person)} {
+      if (!result.contains(item._1)) result(item._1) = HashMap[String,Double]()
+      result(item._1)(person) = prefs(person)(item._1)
+    }
+    result
   }
-  /**
-   * @param args the command line arguments
-   */
+
   def main(args: Array[String]): Unit = {
     import Algorithms._
     println(simDistance(critics, "Lisa Rose", "Gene Seymour"))
     println(simPearson(critics, "Lisa Rose", "Gene Seymour"))
     println(topMatcher(critics, "Toby", n=3))
+    println("------------")
+    println(getRecommendations(critics, "Toby"))
+    println(getRecommendations(critics, "Toby", simDistance))
+    println("------------")
+    println(topMatcher(transformPrefs(critics), "Superman Returns"))
+    println(getRecommendations(transformPrefs(critics), "Just My Luck"))
   }
 
 }
